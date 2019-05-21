873850f7a5d1c88f88207dfc33bd030f690de5da
shifengjin <shifengjin@elements.org.cn>
2019-05-21 21:39:04 +0800

    66
    78

--- test01.py
+++ test01.py
@@ -1,3 +1,13 @@
-print("hello world !!!")
 
-print("NO")
+
+
+
+#  输出99乘法表
+# for i in range(9):
+#     if i != 0:
+#         for j in range(9):
+#             if j != 0:
+#                 print ("%s*%s=%s" % (i,j,i*j),end=" ")
+#                 if i == j :
+#                     break
+#     print("\n")
